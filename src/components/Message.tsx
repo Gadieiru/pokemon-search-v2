66f1619f345697d0124e6596a8af81e7656b1aeb
Gadieiru <diosesmiluz53@gmail.com>
2026-03-26 16:27:43 -0400

    refactor: migrate messages to tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import "../styles/message.css"
+import "../styles/message.css";
 
-export const Message = ({msg, type}) => {
-  return (
-    <div className={`pokemon-message-container ${type}`}>
-        <div className="pokemon-message-content">
-            <p dangerouslySetInnerHTML={{ __html: msg }} />
-        </div>
-    </div>
-  )
+interface MessageProps {
+  msg: string;
+  type?: "error" | "info" | "success" | "warning";
+  bgColor?: string; // Permitimos el color personalizado que envías desde Details
 }
+
+export const Message = ({ msg, type = "info", bgColor }: MessageProps) => {
+
+  const containerStyle = bgColor
+    ? { borderLeftColor: bgColor, backgroundColor: `${bgColor}22` }
+    : {};
+
+  return (
+    <div 
+      className={`pokemon-message-container ${type}`}
+      style={containerStyle}
+      role="alert"
+    >
+      <div className="pokemon-message-content">
+        <p>{msg}</p>
+      </div>
+    </div>
+  );
+};
