62adf49fb8eac92beef73e2024fb440831e49e66
Gadieiru <diosesmiluz53@gmail.com>
2026-03-26 15:39:00 -0400

    refactor: migrate pokemon form to tsx

--- src/components/PokemonForm.tsx
+++ src/components/PokemonForm.tsx
@@ -1,74 +1,41 @@
-import React, { useState } from "react";
-import { PokemonLayout } from "./PokemonLayout";
+import { SearchData } from "../types/pokemon";
 import "../styles/App.css";
 import "../styles/components.css";
 
-const initialForm = {
-  Pokemon: "",
-};
+interface PokemonFormProps {
+  handleSearch: (data: SearchData) => void;
+}
 
-export const PokemonForm = ({ handleSearch }) => {
-  const [form, setForm] = useState(initialForm);
-
-  // Controla el formulario
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      // Objeto que origina el evento :e.target:
-      [e.target.name]: e.target.value, //Su valor sera igual a lo que haya en :e.target.value:
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!form.Pokemon.trim()) {
-      alert("Datos incompletos");
+export const PokemonForm = ({ handleSearch }: PokemonFormProps) => {
+  
+  const clientAction = (formData: FormData) => {
+    const pokemonName = formData.get("Pokemon") as string;
+    
+    if (!pokemonName || !pokemonName.trim()) {
+      alert("Por favor, ingresa el nombre del pokemon");
       return;
     }
-    handleSearch(form);
-    setForm(initialForm);
+    
+    handleSearch({ Pokemon: pokemonName });
   };
 
   return (
       <div className="form_container">
         <h1 className="page_title">¡Bienvenido a la Pokédex!</h1>
-        <form className="pokemon_form" onSubmit={handleSubmit}>
-          <div
-            style={{
-              display: "flex",
-              gap: "10px",
-              marginBottom: "15px",
-              alignSelf: "flex-start",
-            }}
-          >
-            <div
-              style={{
-                width: "15px",
-                height: "15px",
-                borderRadius: "50%",
-                backgroundColor: "#52ff52",
-                border: "2px solid #4c3434",
-              }}
-            ></div>
-            <div
-              style={{
-                width: "15px",
-                height: "15px",
-                borderRadius: "50%",
-                backgroundColor: "#ffff52",
-                border: "2px solid #4c3434",
-              }}
-            ></div>
+
+        <form className="pokemon_form" action={clientAction}>
+          <div>
+            <span className="light light-green"></span>
+            <span className="light light-yellow"></span>
           </div>
 
           <input
             type="text"
             name="Pokemon"
             placeholder="Nombre del pokemon"
-            onChange={handleChange}
-            value={form.Pokemon}
+            autoComplete="off"
           />
+
           <input type="submit" value="Buscar" className="btn-submit" />
         </form>
       </div>
