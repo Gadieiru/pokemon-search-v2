0673d5c7d09d6dd79de15a81a29a80da2b926ab9
Gadieiru <diosesmiluz53@gmail.com>
2026-03-27 15:41:41 -0400

    refactor: migrate loader to tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/animations.css"
 
-export const Loader = () => (
-  <div className="ball"></div>
-);
+export const Loader: React.FC = () => {
+  return (
+    <div className="ball" aria-label="Cargando..."></div>
+  );
+};
