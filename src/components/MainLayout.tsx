b6db8c75e0fe343188920a9e72447b7a99ac0015
Gadieiru <diosesmiluz53@gmail.com>
2026-03-27 16:10:59 -0400

    refactor: migrate main layout to tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,11 +1,13 @@
+import React from "react";
 import { Outlet } from "react-router";
-import { Navbar } from "./Navbar.jsx";
-import { Footer } from "./Footer.jsx";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
-export const MainLayout = () => {
+export const MainLayout: React.FC = () => {
     return(
         <div className="pokedex-page-wrapper">
           <Navbar />
+          
           <main className="main-content">
             <Outlet />
           </main>
